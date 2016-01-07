a847e176cb012c8d0469693961e4e90c8131136d
Martin Jansche <mjansche@google.com>
2016-01-07 18:14:46 +0000

    Make evaluation script suitable for interactive use.

--- xh/evaluate_pronunciation_rules.py
+++ xh/evaluate_pronunciation_rules.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tokenize isiXhosa words according to word-internal capitalization.
+"""Evaluate isiXhosa pronunciation rules against the NCHLT dictionary.
 """
 
 from __future__ import unicode_literals
@@ -46,34 +46,44 @@ def GetSampaToIpaMapping(path):
   return mapping
 
 
-def ReadPronunciationDictionary(f, mapping):
+def TestPronunciationRules(xltor, mapping, dictionary):
+  # Batch testing against a dictionary.
+  with codecs.open(dictionary, 'r', 'utf-8') as reader:
+    for line in reader:
+      line = line.rstrip('\n')
+      fields = line.split('\t')
+      assert len(fields) == 2
+      orth, pron = fields
+      sampa = pron.split()
+      ipa = ''.join(mapping[p] for p in sampa)
+      if orth == 'apreli':  # known issue
+        continue
+      predicted = xltor.transliterate(orth)
+      if predicted != ipa:
+        STDOUT.write('%s\t%s\t%s != %s\n' %
+                     (orth, ' '.join(sampa), ipa, predicted))
+        sys.exit(1)
+  return
+
+
+def ApplyPronunciationRules(xltor, mapping):
+  # For interactive use.
   while True:
-    line = f.readline()
+    line = sys.stdin.readline()
     if not line:
       break
     line = line.decode('utf-8')
-    line = line.rstrip('\n')
-    fields = line.split('\t')
-    assert len(fields) == 2
-    orth, pron = fields
-    sampa = pron.split()
-    ipa = ''.join(mapping[p] for p in sampa)
-    yield orth, sampa, ipa
+    for orth in line.split():
+      predicted = xltor.transliterate(orth)
+      STDOUT.write('%s\t%s\n' % (orth, predicted))
   return
 
 
 if __name__ == '__main__':
-  assert len(sys.argv) == 4
-  argv0, rules, phonemes, dictionary = sys.argv
-
-  xltor = GetPronunciationRules(rules, 'xh-xh_FONIPA')
-  mapping = GetSampaToIpaMapping(phonemes)
-  with open(dictionary, 'r') as f:
-    for orth, sampa, ipa in ReadPronunciationDictionary(f, mapping):
-      if orth == 'apreli':  # known issue
-        continue
-      expected = xltor.transliterate(orth)
-      if expected != ipa:
-        STDOUT.write('%s\t%s\t%s != %s\n' %
-                     (orth, ' '.join(sampa), ipa, expected))
-        sys.exit(1)
+  assert 3 <= len(sys.argv) <= 4
+  xltor = GetPronunciationRules(sys.argv[1], 'xh-xh_FONIPA')
+  mapping = GetSampaToIpaMapping(sys.argv[2])
+  if len(sys.argv) > 3:
+    TestPronunciationRules(xltor, mapping, sys.argv[3])
+  else:
+    ApplyPronunciationRules(xltor, mapping)

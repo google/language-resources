778ced2649af281afc5bd3ca6e80cafcc8d6c9b3
Martin Jansche <mjansche@google.com>
2016-03-26 21:22:11 +0000

    Add singleton version of ValueWeight.

--- festus/weight.h
+++ festus/weight.h
@@ -178,6 +178,136 @@ class ValueWeightStatic {
   ValueType value_;
 };
 
+// OpenFst weight façade for semirings whose elements are passed by value.
+//
+// This version works with a semiring class S with const member functions and
+// creates a singleton instance of S, which it never cleans up. The singleton
+// semiring object should be thread safe.
+template <class S>
+class ValueWeightSingleton {
+ public:
+  typedef typename S::ValueType ValueType;
+  typedef ValueWeightSingleton ReverseWeight;
+
+  ValueWeightSingleton() = default;
+  ~ValueWeightSingleton() = default;
+  ValueWeightSingleton(ValueWeightSingleton &&) = default;
+  ValueWeightSingleton(const ValueWeightSingleton &) = default;
+  ValueWeightSingleton &operator=(ValueWeightSingleton &&) = default;
+  ValueWeightSingleton &operator=(const ValueWeightSingleton &) = default;
+
+  constexpr ValueWeightSingleton(ValueType value) : value_(value) {}
+
+  ValueWeightSingleton &operator=(ValueType value) {
+    value_ = value;
+    return *this;
+  }
+
+  ValueType Value() const { return value_; }
+
+  static const ValueWeightSingleton NoWeight() {
+    return ValueWeightSingleton(kInstance->NoWeight());
+  }
+
+  static constexpr ValueWeightSingleton Zero() {
+    return ValueWeightSingleton(kInstance->Zero());
+  }
+
+  static constexpr ValueWeightSingleton One() {
+    return ValueWeightSingleton(kInstance->One());
+  }
+
+  friend inline ValueWeightSingleton Plus(
+      ValueWeightSingleton lhs,
+      ValueWeightSingleton rhs) {
+    lhs.value_ = kInstance->OpPlus(lhs.value_, rhs.value_);
+    return lhs;
+  }
+
+  friend inline ValueWeightSingleton Minus(
+      ValueWeightSingleton lhs,
+      ValueWeightSingleton rhs) {
+    lhs.value_ = kInstance->OpMinus(lhs.value_, rhs.value_);
+    return lhs;
+  }
+
+  friend inline ValueWeightSingleton Times(
+      ValueWeightSingleton lhs,
+      ValueWeightSingleton rhs) {
+    lhs.value_ = kInstance->OpTimes(lhs.value_, rhs.value_);
+    return lhs;
+  }
+
+  friend inline ValueWeightSingleton Divide(
+      ValueWeightSingleton lhs,
+      ValueWeightSingleton rhs,
+      fst::DivideType typ = fst::DIVIDE_ANY) {
+    lhs.value_ = kInstance->OpDivide(lhs.value_, rhs.value_);
+    return lhs;
+  }
+
+  friend inline ValueWeightSingleton Star(
+      ValueWeightSingleton w) {
+    w.value_ = kInstance->OpStar(w.value_);
+    return w;
+  }
+
+  ValueWeightSingleton Reverse() const {
+    return ValueWeightSingleton(kInstance->Reverse(value_));
+  }
+
+  ValueWeightSingleton Quantize(float delta = fst::kDelta) const {
+    return ValueWeightSingleton(kInstance->Quantize(value_, delta));
+  }
+
+  constexpr bool Member() const { return kInstance->Member(value_); }
+
+  friend inline bool operator==(
+      ValueWeightSingleton lhs,
+      ValueWeightSingleton rhs) {
+    return kInstance->EqualTo(lhs.value_, rhs.value_);
+  }
+
+  friend inline bool operator!=(
+      ValueWeightSingleton lhs,
+      ValueWeightSingleton rhs) {
+    return !kInstance->EqualTo(lhs.value_, rhs.value_);
+  }
+
+  friend inline bool ApproxEqual(
+      ValueWeightSingleton lhs,
+      ValueWeightSingleton rhs,
+      float delta = fst::kDelta) {
+    return kInstance->ApproxEqualTo(lhs.value_, rhs.value_, delta);
+  }
+
+  std::size_t Hash() const { return std::hash<ValueType>()(value_); }
+
+  std::istream &Read(std::istream &strm) {
+    static_assert(std::is_pod<ValueType>::value, "value type should be POD");
+    return strm.read(reinterpret_cast<char *>(&value_), sizeof(value_));
+  }
+
+  std::ostream &Write(std::ostream &strm) const {
+    return strm.write(reinterpret_cast<const char *>(&value_), sizeof(value_));
+  }
+
+  static const string &Type() {
+    static const string type = kInstance->Name() +
+        (sizeof(value_) == 4 ? "" : PrecisionString<sizeof(value_)>::Get());
+    return type;
+  }
+
+  static constexpr uint64 Properties() { return kInstance->Properties(); }
+
+ private:
+  static const S *const kInstance;  // Owned singleton, will never be deleted.
+  ValueType value_;
+};
+
+template <class S>
+const S *const ValueWeightSingleton<S>::kInstance = new S();
+
 }  // namespace festus
 
 #endif  // FESTUS_WEIGHT_H__

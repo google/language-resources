google/language-resources

commit 3c9c06e25b481917fe473046d1360e609eb9c9bf

Rename and PathsToVector to clarify assumptions about its FST parameter.

festus/runtime/fst-util.h
@@ -21,7 +21,6 @@
 #ifndef FESTUS_RUNTIME_FST_UTIL_H__
 #define FESTUS_RUNTIME_FST_UTIL_H__
 
-#include <cmath>
 #include <cstddef>
 #include <iterator>
 #include <limits>
@@ -295,18 +294,19 @@ void DeterminizeConvertWeight(
 }
 
 // Converts the output FST of ShortestPath() into vector form.
+// Returns a vector holding the output string and weight of each successful
+// path in the FST.
 template <class F>
-void PathsToVector(
+std::vector<std::pair<string, float>> ShortestPathsToVector(
     const F &paths_fst,
-    std::vector<std::pair<string, float>> *paths,
     typename F::Arc::Weight total_weight = F::Arc::Weight::One()) {
   typedef typename F::Arc Arc;
   typedef typename F::StateId StateId;
   typedef typename F::Weight Weight;
-  paths->clear();
+  std::vector<std::pair<string, float>> paths;
   const StateId start = paths_fst.Start();
-  if (start == fst::kNoStateId) return;
-  paths->reserve(paths_fst.NumArcs(start));
+  if (start == fst::kNoStateId) return paths;
+  paths.reserve(paths_fst.NumArcs(start));
   const fst::SymbolTable *symbols = paths_fst.OutputSymbols();
   CHECK(symbols != nullptr);
   typedef fst::ArcIterator<fst::VectorFst<Arc>> MyArcIterator;
@@ -329,10 +329,9 @@ void PathsToVector(
       CHECK((iter.Next(), iter.Done()));
     }
     CHECK(fst::ArcIterator<fst::VectorFst<Arc>>(paths_fst, state).Done());
-    weight = Times(weight, paths_fst.Final(state));
-    weight = Divide(weight, total_weight);
-    paths->emplace_back(std::move(str), std::exp(-weight.Value()));
+    paths.emplace_back(std::move(str), weight.Value());
   }
+  return paths;
 }
 
 constexpr uint64 kConnected = fst::kAccessible | fst::kCoAccessible;

festus/runtime/g2p.h
@@ -255,7 +255,10 @@ bool G2P<Arc>::Pronounce(const string &spelling,
   }
 
   VLOG(2) << "8. Convert shortest paths to pronunciations.";
-  PathsToVector(paths, &result->pronunciations, total_weight.Value());
+  result->pronunciations = ShortestPathsToVector(paths, total_weight.Value());
+  for (auto &pron : result->pronunciations) {
+    pron.second = std::exp(-pron.second);
+  }
   result->error.clear();
   return true;
 }

commit f9211a7bbe1708988766b845284d5c4b266392f1

Fix bugs introduced in commit 3c9c06e: account for final weight in path weight; normalize path probabilities.

festus/runtime/fst-util.h
@@ -298,8 +298,7 @@ void DeterminizeConvertWeight(
 // path in the FST.
 template <class F>
 std::vector<std::pair<string, float>> ShortestPathsToVector(
-    const F &paths_fst,
-    typename F::Arc::Weight total_weight = F::Arc::Weight::One()) {
+    const F &paths_fst) {
   typedef typename F::Arc Arc;
   typedef typename F::StateId StateId;
   typedef typename F::Weight Weight;
@@ -329,6 +328,7 @@ std::vector<std::pair<string, float>> ShortestPathsToVector(
       CHECK((iter.Next(), iter.Done()));
     }
     CHECK(fst::ArcIterator<fst::VectorFst<Arc>>(paths_fst, state).Done());
+    weight = Times(weight, paths_fst.Final(state));
     paths.emplace_back(std::move(str), weight.Value());
   }
   return paths;

festus/runtime/g2p.h
@@ -255,9 +255,9 @@ bool G2P<Arc>::Pronounce(const string &spelling,
   }
 
   VLOG(2) << "8. Convert shortest paths to pronunciations.";
-  result->pronunciations = ShortestPathsToVector(paths, total_weight.Value());
+  result->pronunciations = ShortestPathsToVector(paths);
   for (auto &pron : result->pronunciations) {
-    pron.second = std::exp(-pron.second);
+    pron.second = std::exp(total_weight.Value() - pron.second);
   }
   result->error.clear();
   return true;
4283fea2fc89990452646b01ccd9832f4c559fa3
Martin Jansche <mjansche@google.com>
2016-06-05 11:53:18 +0100

    Tweak g2p script.

--- my/g2p.sh
+++ my/g2p.sh
@@ -4,8 +4,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-runfiles="${0}.runfiles"
+projdir="${0}.runfiles"
 
-"$runfiles/utils/thrax_g2p" \
-  --fst="$runfiles/my/G2P.fst" \
-  --phoneme_syms="$runfiles/my/ipa_phoneme.syms"
+exec "$projdir/utils/thrax_g2p" \
+  --fst="$projdir/my/G2P.fst" \
+  --phoneme_syms="$projdir/my/ipa_phoneme.syms"

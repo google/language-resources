bf4ef042932f8309659ff1861de722c7eb5ec51a
Martin Jansche <mjansche@google.com>
2016-11-12 22:29:27 -0500

    Generalize G2P data preparation.

--- af/prepare-g2p-data.sh
+++ af/prepare-g2p-data.sh
@@ -4,25 +4,25 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 OUTPUT_DIRECTORY" >&2
+if [ "$#" -ne 4 ]; then
+  echo "Usage: $0 DICTIONARY ALIGNABLES TEST_WORDS OUTPUT_DIRECTORY" >&2
   exit 2
 fi
 
-outdir="$1"
+# Input files and output directory:
+dict="$1"
+alignables="$2"
+words="$3"
+outdir="$4"
 
 # Output files that will be generated in $outdir:
 syms="$outdir/graphone.syms"
 train="$outdir/g2p_train.far"
 test="$outdir/g2p_test.tsv"
 
-# Input files and tools:
+# Tools:
 runfiles="${0}.runfiles"
-projdir="$runfiles/language_resources"
-dict="$projdir/af/lex_regular.txt"
-alignables="$projdir/af/alignables.txt"
-words="$projdir/af/test_words.txt"
-festus="$projdir/festus"
+festus="$runfiles/language_resources/festus"
 openfst="$runfiles/openfst"
 
 export LC_ALL=C.UTF-8

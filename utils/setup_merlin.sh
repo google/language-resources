4d3f8268b126e7bd27379abd519cbff622342dcf
Pasindu De Silva <pasindu@google.com>
2017-08-08 00:47:38 +0800

    Fixed sample rate and tools directory, use python feature extraction

--- utils/setup_merlin.sh
+++ utils/setup_merlin.sh
@@ -98,7 +98,7 @@ cp "${FESTIVAL_VOICE_PATH}"/wav/*.wav "${MERLIN_DATA_PATH_WAV}"
 
 # TODO(pasindu): Switch to python script.
 echo "Extracting acoustic features."
-"${MERLIN_PATH}/misc/scripts/vocoder/world/extract_features_for_merlin.sh" "${MERLIN_PATH}" \
+python "${MERLIN_PATH}/misc/scripts/vocoder/world/extract_features_for_merlin.py" "${MERLIN_PATH}" \
   "${WAV_PATH}" "${MERLIN_DATA_PATH}" "${SAMPLE_RATE}"
 
 echo "Preparing to generate HTS labels files from the given festival voice."
@@ -171,16 +171,18 @@ echo "TEST_ACOUSTIC_CONF_FILE - ${TEST_ACOUSTIC_CONF_FILE}"
 
 sed -i "s|MERLIN_PATH|${MERLIN_PATH}|g" "${ACOUSTIC_CONF_FILE}"
 sed -i "s|MERLIN_TOPLEVEL_PATH|${MERLIN_VOICE_PATH}|g" "${ACOUSTIC_CONF_FILE}"
+sed -i "s|SAMPLE_RATE|${SAMPLE_RATE}|g" "${ACOUSTIC_CONF_FILE}"
 
 sed -i "s|MERLIN_PATH|${MERLIN_PATH}|g" "${DURATION_CONF_FILE}"
 sed -i "s|MERLIN_TOPLEVEL_PATH|${MERLIN_VOICE_PATH}|g" "${DURATION_CONF_FILE}"
+sed -i "s|SAMPLE_RATE|${SAMPLE_RATE}|g" "${DURATION_CONF_FILE}"
 
 sed -i "s|MERLIN_PATH|${MERLIN_PATH}|g" "${TEST_DURATION_CONF_FILE}"
 sed -i "s|MERLIN_TOPLEVEL_PATH|${MERLIN_VOICE_PATH}|g" "${TEST_DURATION_CONF_FILE}"
 
 sed -i "s|MERLIN_PATH|${MERLIN_PATH}|g" "${TEST_ACOUSTIC_CONF_FILE}"
 sed -i "s|MERLIN_TOPLEVEL_PATH|${MERLIN_VOICE_PATH}|g" "${TEST_ACOUSTIC_CONF_FILE}"
-
+sed -i "s|SAMPLE_RATE|${SAMPLE_RATE}|g" "${TEST_ACOUSTIC_CONF_FILE}"
 
 MERLIN_FILE_LIST_PATH="${MERLIN_DATA_PATH}/file_id_list.scp"
 echo "Copying filelist ${FESTIVAL_VOICE_FILE_LIST_PATH}."

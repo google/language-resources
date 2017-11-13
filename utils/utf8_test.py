3be04eabb4c5bce8441f415b755c22a5a9b64bef
Martin Jansche <mjansche@google.com>
2017-11-12 21:15:39 -0500

    Better testing of utf8.py; change error handling for Print().

--- utils/utf8_test.py
+++ utils/utf8_test.py
@@ -1,11 +1,34 @@
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from __future__ import print_function
 
 import sys
 
+# Special unqualified local import, for use with utf8_py2and3_test:
 import utf8
 
 
+class Foo(object):
+
+  def __str__(self):
+    return 'Foo'
+
+
 def test_print(p):
+  p(True, 42, 3.14, None, [], Foo())
+
   p(u'hello', u'world')
   p(u'hello', b'world')
   p(b'hello', u'world')
@@ -17,8 +40,9 @@ def test_print(p):
   p(b'\xc2\xa1Hola!', u'\xa1mundo!')
   p(b'\xc2\xa1Hola!', b'\xc2\xa1mundo!')
 
-  # Test an invalid UTF-8 byte sequence:
-  p(b'\xa1', b'Hola!')
+  # Test invalid UTF-8 byte sequences:
+  p(b'\xc2Hola!', u'\xa1mundo!')
+  p(b'\xa1Hola!', u'\xa1mundo!')
   return
 
 
@@ -30,6 +54,7 @@ def main(argv):
     def p(*values):
       utf8.Print(*values)
   test_print(p)
+  sys.stdout.flush()
   return
 
 

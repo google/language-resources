9030040028a1b220d7b1dd0f7876a5ddd0879bec
Martin Jansche <mjansche@google.com>
2017-11-12 21:47:48 -0500

    Python 3 compatibility, continued.

--- utils/festival_lexicon_from_tsv.py
+++ utils/festival_lexicon_from_tsv.py
@@ -1,7 +1,4 @@
-#! /usr/bin/python2 -u
-# -*- coding: utf-8 -*-
-#
-# Copyright 2016 Google Inc. All Rights Reserved.
+# Copyright 2016, 2017 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,36 +18,25 @@
 from __future__ import unicode_literals
 
 import codecs
-import sys
 
-STDIN = codecs.getreader('utf-8')(sys.stdin)
-STDOUT = codecs.getwriter('utf-8')(sys.stdout)
-STDERR = codecs.getwriter('utf-8')(sys.stderr)
-
-
-def GetlineUnbuffered(f=sys.stdin):
-  while True:
-    line = f.readline()
-    if not line:
-      break
-    yield line.decode('utf-8')
-  return
+from utils import utf8
 
 
 def main(unused_args):
-  STDOUT.write('MNCL\n')
-  for line in GetlineUnbuffered():
+  utf8.Print('MNCL')
+  for line in utf8.stdin:
     line = line.rstrip('\n')
     # Skip comments
-    if len(line) == 0 or line[0] == "#":
+    if not line or line.startswith('#'):
       continue
     fields = line.split('\t')
     assert len(fields) >= 2
     orth = fields[0]
-    sylls = ' '.join('((%s) 1)' % s for s in fields[1].split(' . '))
-    STDOUT.write('("%s" nil (%s))\n' % (orth, sylls))
+    pron = fields[1]
+    sylls = ' '.join('((%s) 1)' % s for s in pron.split(' . '))
+    utf8.Print('("%s" nil (%s))' % (orth, sylls))
   return
 
 
 if __name__ == '__main__':
-  main(sys.argv)
+  main(None)

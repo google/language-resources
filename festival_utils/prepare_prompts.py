12d84d0ce4352b7ca562c8dd3de23a714e1391aa
Martin Jansche <mjansche@google.com>
2017-11-28 13:19:36 -0500

    Python 3 compatibility
    Make festival_utils/prepare_prompts.py safe for Python 2 and 3.

--- festival_utils/prepare_prompts.py
+++ festival_utils/prepare_prompts.py
@@ -22,29 +22,20 @@ Removes prompts without full lexicon coverage.
 
 from __future__ import unicode_literals
 
-import codecs
+import io
 import sys
 
-STDIN = codecs.getreader('utf-8')(sys.stdin)
-STDOUT = codecs.getwriter('utf-8')(sys.stdout)
-STDERR = codecs.getwriter('utf-8')(sys.stderr)
+STDIN = io.open(0, mode='rt', encoding='utf-8', closefd=False)
+STDOUT = io.open(1, mode='wt', encoding='utf-8', closefd=False)
+STDERR = io.open(2, mode='wt', encoding='utf-8', closefd=False)
 
 
 def ReadWords(path):
-  with codecs.open(path, 'r', 'utf-8') as reader:
+  with io.open(path, mode='rt', encoding='utf-8') as reader:
     for line in reader:
       fields = line.split()
       if not fields: continue
       yield fields[0]
-  return
-
-
-def GetlineUnbuffered(f=sys.stdin):
-  while True:
-    line = f.readline()
-    if not line:
-      break
-    yield line.decode('utf-8')
   return
 
 
@@ -55,7 +46,7 @@ def main(argv):
 
   vocabulary = frozenset(ReadWords(argv[1]))
 
-  for line in GetlineUnbuffered():
+  for line in STDIN:
     line = line.rstrip('\n')
     fields = line.split('\t')
     assert len(fields) >= 2

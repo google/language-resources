ba38372a7ac83e8c1b27b3a3dff752a829b283cc
Martin Jansche <mjansche@google.com>
2017-11-29 21:07:34 +0000

    Travis CI: Factor out script, use only 2 Pythons
    Move Travis script logic into a dedicated shell script.
    
    Reduce the number of Python versions in build matrix. There was no
    reason to go for the Full Monty of all Pythons, as this greatly
    increased the total runtime and did not provide useful information.
    
    Settled on Python 2.7 and 3.3, as 2.7 is the final minor version of
    Python 2 and 3.3 is the minimally required minor version of Python 3.

---
+++ travis_script.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+set -o nounset
+
+PYTHON_EXECUTABLE="${PYTHON_EXECUTABLE:-$(which python)}"
+
+BAZEL_EXECUTABLE="${BAZEL_EXECUTABLE:-$(which bazel)}"
+
+# Not having a host configuration which is distinct from the target
+# configuration means that portions of tools (e.g. protobuf) that are shared
+# between build time (protoc, compiled for host) and runtime (protobuf runtime
+# library, compiled for target) only need to be built once. For a one-shot
+# continuous integration build, this saves a little bit of total build time.
+BAZEL_STRATEGY='--nodistinct_host_configuration'
+
+BAZEL_PYTHON="$("$BAZEL_EXECUTABLE" canonicalize-flags -- \
+  --python_path="$PYTHON_EXECUTABLE" 2>/dev/null)"
+
+# As of this writing (Bazel 0.8.0 was released a few days ago), Bazel's Android
+# tools (@bazel_tools//tools/android/...) do not work with Python 3, so we build
+# Android targets in //Mymr/... without setting --python_path (configured by
+# $BAZEL_PYTHON).
+
+set -o errexit
+set -o xtrace
+"$BAZEL_EXECUTABLE" run   $BAZEL_STRATEGY $BAZEL_PYTHON //utils:python_version
+"$BAZEL_EXECUTABLE" test  $BAZEL_STRATEGY $BAZEL_PYTHON -- //... -//Mymr/...
+"$BAZEL_EXECUTABLE" build $BAZEL_STRATEGY                         //Mymr/...

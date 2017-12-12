fcd6af0a2a02ef87eedd78aa3c09836cc0799a29
Martin Jansche <mjansche@google.com>
2017-12-12 21:00:40 +0000

    Print ICU and Unicode version, if available

--- utils/python_version.py
+++ utils/python_version.py
@@ -4,6 +4,14 @@
 
 import sys
 
-sys.stdout.write(sys.executable + '\n')
-sys.stdout.write(sys.version + '\n')
+sys.stdout.write('%s\n' % sys.executable)
+sys.stdout.write('%s\n' % sys.version)
+
+try:
+  import icu  # pylint: disable=g-import-not-at-top
+  sys.stdout.write('ICU %s\n' % icu.ICU_VERSION)
+  sys.stdout.write('Unicode %s\n' % icu.UNICODE_VERSION)
+except ImportError:
+  pass
+
 sys.stdout.flush()

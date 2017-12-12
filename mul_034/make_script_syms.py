ee17990625c652181130f216ee414ff397b2a8ea
Martin Jansche <mjansche@google.com>
2017-12-12 19:18:23 +0000

    Further tweak SymbolTable generation script
    Include Sinhala alternative names. Sinhala short names are then derived
    in a standard fashion from alternative (Sanskritic) names. The official
    (Sinhalese) names do not match the conventions for the other Indic
    scripts.
    
    Do not mangle names of vocalic consonants. We now write "vocalic_r"
    instead of "ri" to make it easier to match the short name with the
    Unicode name. The previous short name "ri" was intended as a quick
    transliteration, but as we are replacing this with a more systematic
    transliteration approach, this use case is no longer relevant.
    
    Always include ZWNJ and ZWJ in every script symbol table.

--- mul_034/make_script_syms.py
+++ mul_034/make_script_syms.py
@@ -55,7 +55,6 @@ STOPWORDS = frozenset([
     'SYLLABLE',
     'SYMBOL',
     'TONE',
-    'VOCALIC',
     'VOWEL',
 ])
 
@@ -82,6 +81,88 @@ EXCEPTIONS = {
     0x11131: '-o_mark',
     0x11132: '-au_mark',
     0x111CB: 'vowel_modifier',
+}
+
+SINHALA_CHAR_NAMES = {
+    0x0D82: 'sinhala sign anusvara',
+    0x0D83: 'sinhala sign visarga',
+    0x0D85: 'sinhala letter a',
+    0x0D86: 'sinhala letter aa',
+    0x0D87: 'sinhala letter ae',
+    0x0D88: 'sinhala letter aae',
+    0x0D89: 'sinhala letter i',
+    0x0D8a: 'sinhala letter ii',
+    0x0D8b: 'sinhala letter u',
+    0x0D8c: 'sinhala letter uu',
+    0x0D8d: 'sinhala letter vocalic r',
+    0x0D8e: 'sinhala letter vocalic rr',
+    0x0D8f: 'sinhala letter vocalic l',
+    0x0D90: 'sinhala letter vocalic ll',
+    0x0D91: 'sinhala letter e',
+    0x0D92: 'sinhala letter ee',
+    0x0D93: 'sinhala letter ai',
+    0x0D94: 'sinhala letter o',
+    0x0D95: 'sinhala letter oo',
+    0x0D96: 'sinhala letter au',
+    0x0D9a: 'sinhala letter ka',
+    0x0D9b: 'sinhala letter kha',
+    0x0D9c: 'sinhala letter ga',
+    0x0D9d: 'sinhala letter gha',
+    0x0D9e: 'sinhala letter nga',
+    0x0D9F: 'sinhala letter nnga',
+    0x0DA0: 'sinhala letter ca',
+    0x0DA1: 'sinhala letter cha',
+    0x0DA2: 'sinhala letter ja',
+    0x0DA3: 'sinhala letter jha',
+    0x0DA4: 'sinhala letter nya',
+    0x0DA5: 'sinhala letter jnya',
+    0x0DA6: 'sinhala letter nyja',
+    0x0DA7: 'sinhala letter tta',
+    0x0DA8: 'sinhala letter ttha',
+    0x0DA9: 'sinhala letter dda',
+    0x0DAA: 'sinhala letter ddha',
+    0x0DAB: 'sinhala letter nna',
+    0x0DAC: 'sinhala letter nndda',
+    0x0DAD: 'sinhala letter ta',
+    0x0DAE: 'sinhala letter tha',
+    0x0DAF: 'sinhala letter da',
+    0x0DB0: 'sinhala letter dha',
+    0x0DB1: 'sinhala letter na',
+    0x0DB3: 'sinhala letter nda',
+    0x0DB4: 'sinhala letter pa',
+    0x0DB5: 'sinhala letter pha',
+    0x0DB6: 'sinhala letter ba',
+    0x0DB7: 'sinhala letter bha',
+    0x0DB8: 'sinhala letter ma',
+    0x0DB9: 'sinhala letter mba',
+    0x0DBA: 'sinhala letter ya',
+    0x0DBB: 'sinhala letter ra',
+    0x0DBD: 'sinhala letter la',
+    0x0DC0: 'sinhala letter va',
+    0x0DC1: 'sinhala letter sha',
+    0x0DC2: 'sinhala letter ssa',
+    0x0DC3: 'sinhala letter sa',
+    0x0DC4: 'sinhala letter ha',
+    0x0DC5: 'sinhala letter lla',
+    0x0DC6: 'sinhala letter fa',
+    0x0DCA: 'sinhala sign virama',
+    0x0DCF: 'sinhala vowel sign aa',
+    0x0DD0: 'sinhala vowel sign ae',
+    0x0DD1: 'sinhala vowel sign aae',
+    0x0DD2: 'sinhala vowel sign i',
+    0x0DD3: 'sinhala vowel sign ii',
+    0x0DD4: 'sinhala vowel sign u',
+    0x0DD6: 'sinhala vowel sign uu',
+    0x0DD8: 'sinhala vowel sign vocalic r',
+    0x0DD9: 'sinhala vowel sign e',
+    0x0DDA: 'sinhala vowel sign ee',
+    0x0DDB: 'sinhala vowel sign ai',
+    0x0DDC: 'sinhala vowel sign o',
+    0x0DDD: 'sinhala vowel sign oo',
+    0x0DDE: 'sinhala vowel sign au',
+    0x0DDF: 'sinhala vowel sign vocalic l',
+    0x0DF2: 'sinhala vowel sign vocalic rr',
+    0x0DF3: 'sinhala vowel sign vocalic ll',
 }
 
 BRAHMIC = [
@@ -140,6 +221,13 @@ BRAHMIC = [
 ]
 
 
+def CharName(c):
+  cp = ord(c)
+  if cp in SINHALA_CHAR_NAMES:
+    return SINHALA_CHAR_NAMES[cp].upper()
+  return icu.Char.charName(c)
+
+
 def SymbolNames(script, include_script_code=False):
   """Yields short symbolic names for all characters in the given script."""
   script_name = script.getName()
@@ -148,7 +236,7 @@ def SymbolNames(script, include_script_code=False):
     if ord(c) in EXCEPTIONS:
       short_name = EXCEPTIONS[ord(c)]
     else:
-      name = icu.Char.charName(c)
+      name = CharName(c)
       if not name:
         continue
       prefix = script_name.upper().replace('_', ' ')
@@ -160,11 +248,9 @@ def SymbolNames(script, include_script_code=False):
       components = [t for t in name.split() if t not in STOPWORDS]
       short_name = '_'.join(components).lower()
       assert short_name, ('Empty symbolic name for %04X (%s)' %
-                          (ord(c), icu.Char.charName(c)))
+                          (ord(c), CharName(c)))
       if short_name.startswith('-'):
         short_name = "'%s" % short_name[1:]
-      if 'VOCALIC' in name:
-        short_name += 'i'
       if ('VOWEL SIGN' in name or 'CONSONANT SIGN' in name or 'MARK' in name or
           'MEDIAL' in name or 'COMBINING' in name or 'SUBJOINED' in name or
           'SARA' in name):
@@ -172,6 +258,8 @@ def SymbolNames(script, include_script_code=False):
     if include_script_code:
       short_name = '%s:%s' % (script.getShortName(), short_name)
     yield c, short_name
+  yield '\u200C', 'ZWNJ'
+  yield '\u200D', 'ZWJ'
   return
 
 
@@ -192,8 +280,8 @@ def IsBijectiveMapping(syms):
     if sym in sym2char:
       b = sym2char[sym]
       STDERR.write('%04X (%s) and %04X (%s) both map to "%s"\n' %
-                   (ord(b), icu.Char.charName(b),
-                    ord(c), icu.Char.charName(c), sym))
+                   (ord(b), CharName(b),
+                    ord(c), CharName(c), sym))
       ok = False
     else:
       sym2char[sym] = c

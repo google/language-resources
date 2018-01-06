e9a62226815788a3de1241da5a0979012da646df
Martin Jansche <mjansche@google.com>
2018-01-06 23:41:50 +0000

    Python 2.6 compatibility
    Test against system Python (2.6, 2.7) on Darwin.
    Add test coverage for characters outside the BMP.

--- utils/utf8_test.sh
+++ utils/utf8_test.sh
@@ -6,12 +6,14 @@
 set -o errexit
 set -o nounset
 
-for py in python python2 python3; do
+for py in /usr/bin/python /usr/bin/python2.6 /usr/bin/python2.7 \
+          python python2 python2.6 python2.7 python3; do
   if which $py; then
+    py_binary="$(which $py)"
     for e in '' '-' 'LC_CTYPE=C' 'PYTHONIOENCODING=ASCII' \
       'PYTHONIOENCODING=Latin-1'; do
-      env $e $py -c 'import sys; print(sys.stdout.encoding)'
-      env $e $py "$1" | diff -u - "$2"
+      env $e $py_binary -c 'import sys; print(sys.stdout.encoding)'
+      env $e $py_binary "$1" | diff -u - "$2"
     done
   fi
 done

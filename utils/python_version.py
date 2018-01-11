536e261b2a80139718fddc1cccdc78ba4f067fdc
Martin Jansche <mjansche@google.com>
2018-01-11 12:01:48 +0000

    More details in Python version diagnostics
    Also expose it as a py_test rule.

--- utils/python_version.py
+++ utils/python_version.py
@@ -7,11 +7,20 @@ import sys
 sys.stdout.write('%s\n' % sys.executable)
 sys.stdout.write('%s\n' % sys.version)
 
+if sys.maxunicode == 0xFFFF:
+  sys.stdout.write('Narrow (ucs2) Unicode build\n')
+elif sys.maxunicode == 0x10FFFF:
+  sys.stdout.write('Wide (ucs4) Unicode build\n')
+else:
+  sys.stdout.write('Unknown Unicode build\n')
+
 try:
   import icu  # pylint: disable=g-import-not-at-top
-  sys.stdout.write('ICU %s\n' % icu.ICU_VERSION)
-  sys.stdout.write('Unicode %s\n' % icu.UNICODE_VERSION)
+  sys.stdout.write('Unicode %s (ICU %s) via module icu\n' %
+                   (icu.UNICODE_VERSION, icu.ICU_VERSION))
 except ImportError:
-  pass
+  import unicodedata  # pylint: disable=g-import-not-at-top
+  sys.stdout.write('Unicode %s via module unicodedata\n' %
+                   unicodedata.unidata_version)
 
 sys.stdout.flush()

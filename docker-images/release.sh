bec0c4353bec40d7356a84d49fcd0421296d45eb
Pasindu De Silva <pasindu@google.com>
2018-01-18 10:48:06 +0800

    Use 4 cpus during docker build

--- docker-images/release.sh
+++ docker-images/release.sh
@@ -5,7 +5,7 @@ USERNAME="langtech"
 for IMAGE_NAME in language-resources festival merlin; do
   cd ${IMAGE_NAME}
   IMAGE_NAME="base-${IMAGE_NAME}"
-  docker build --no-cache -t ${USERNAME}/${IMAGE_NAME}:latest .
+  docker build --cpuset-cpus=4  --no-cache -t ${USERNAME}/${IMAGE_NAME}:latest .
   docker push ${USERNAME}/${IMAGE_NAME}
   cd ../
 done

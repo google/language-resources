ca8bec77b32763d4314f1190ff45e75486341161
Martin Jansche <mjansche@google.com>
2018-01-26 19:19:06 +0000

    Travis CI: Simpler config for branch fonbund
    Re-enable Travis CI testing based on master branch, but drop everything
    not related to fonbund.

--- travis/script.sh
+++ travis/script.sh
@@ -1,29 +1,17 @@
 #! /bin/bash
 
-if [ -z "$ANDROID_HOME" ]; then
-  echo 'Building the Android parts of this project requires ANDROID_HOME'
-  echo 'to point to the Android SDK, e.g. in $HOME/Android/Sdk'
-fi
-
 BAZEL_EXECUTABLE="${BAZEL_EXECUTABLE:-$(which bazel)}"
 if [ -z "$BAZEL_EXECUTABLE" ]; then
   echo 'No bazel executable found or configured.'
   exit 1
 fi
 
-# Not having a host configuration which is distinct from the target
-# configuration means that portions of tools (e.g. protobuf) that are shared
-# between build time (protoc, compiled for host) and runtime (protobuf runtime
-# library, compiled for target) only need to be built once. For a one-shot
-# continuous integration build, this saves a little bit of total build time.
-STRATEGY='--nodistinct_host_configuration'
+STRATEGY=''
 STARTUP=''
-
 if [ -n "$TRAVIS" ]; then
   STARTUP+=' --batch'
   STRATEGY+=' --jobs=2'
   STRATEGY+=' --noshow_progress'
-  STRATEGY+=' --test_timeout_filters=-long'
 fi
 
 SHOW='--nocache_test_results --test_output=all'

1a5f4aed69687a06f18ca8287191338f60b2f4b3
Pasindu De Silva <pasindu@google.com>
2018-01-29 15:52:09 +0800

    Tag docker images with build time

--- docker-images/release.sh
+++ docker-images/release.sh
@@ -1,11 +1,12 @@
 set -x 
 
 USERNAME="langtech"
+BUILD_TIME=$(date +%s)
 
 for IMAGE_NAME in language-resources festival merlin; do
   cd ${IMAGE_NAME}
   IMAGE_NAME="base-${IMAGE_NAME}"
-  docker build --cpuset-cpus=4  --no-cache -t ${USERNAME}/${IMAGE_NAME}:latest .
+  docker build --cpuset-cpus=4  --no-cache -t ${USERNAME}/${IMAGE_NAME}:${BUILD_TIME} .
   docker push ${USERNAME}/${IMAGE_NAME}
   cd ../
 done

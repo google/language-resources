25d17f384f57a35bc57c36d745d8629dd167a95a
Pasindu De Silva <pasindu@google.com>
2018-02-27 15:13:30 +0800

    Update docker tags to v1

--- docker-images/release.sh
+++ docker-images/release.sh
@@ -1,12 +1,12 @@
 set -x 
 
 USERNAME="langtech"
-BUILD_TIME=v1
+BUILD_TAG=v1
 
 for IMAGE_NAME in language-resources festival merlin; do
   cd ${IMAGE_NAME}
   IMAGE_NAME="base-${IMAGE_NAME}"
-  docker build --cpuset-cpus=4  --no-cache -t ${USERNAME}/${IMAGE_NAME}:${BUILD_TIME} .
+  docker build --cpuset-cpus=4  --no-cache -t ${USERNAME}/${IMAGE_NAME}:${BUILD_TAG} .
   docker push ${USERNAME}/${IMAGE_NAME}
   cd ../
 done

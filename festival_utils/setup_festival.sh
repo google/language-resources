d22f1f7b5b42c39e8dcae1b1af844b08c166440b
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-02-19 08:28:39 +0700

    Update setup_festival.sh

--- festival_utils/setup_festival.sh
+++ festival_utils/setup_festival.sh
@@ -6,20 +6,22 @@ cd "${FESTIVAL_SUIT_PATH}"
 echo "Setting up at festival at ${FESTIVAL_SUIT_PATH:?Set env variable FESTIVAL_SUIT_PATH}"
 
 echo "Downloading up festival and friends."
-curl -L http://festvox.org/packed/festival/2.4/festlex_CMU.tar.gz | \
+curl -L http://festvox.org/packed/festival/2.5/festlex_CMU.tar.gz | \
 tar xz --no-same-owner --no-same-permissions && \
-curl -L http://festvox.org/packed/festival/2.4/festlex_POSLEX.tar.gz | \
+curl -L http://festvox.org/packed/festival/2.5/festlex_POSLEX.tar.gz | \
 tar xz --no-same-owner --no-same-permissions && \
-curl -L http://festvox.org/packed/festival/2.4/voices/festvox_kallpc16k.tar.gz | \
+curl -L http://festvox.org/packed/festival/2.5/voices/festvox_kallpc16k.tar.gz | \
 tar xz --no-same-owner --no-same-permissions && \
-curl -L http://festvox.org/festvox-2.7/festvox-2.7.0-release.tar.gz | \
+curl -L http://festvox.org/packed/festvox/2.8/festvox-2.8.0-release.tar.gz | \
 tar xz --no-same-owner --no-same-permissions && \
-curl -L https://download.sourceforge.net/project/sp-tk/SPTK/SPTK-3.6/SPTK-3.6.tar.gz | \
+curl -L https://download.sourceforge.net/project/sp-tk/SPTK/SPTK-3.11/SPTK-3.11.tar.gz | \
 tar xz --no-same-owner --no-same-permissions && \
 patch -p0 < festvox/src/clustergen/SPTK-3.6.patch
-curl -L http://festvox.org/packed/festival/2.4/speech_tools-2.4-release.tar.gz | \
+curl -L http://festvox.org/packed/festival/2.5/speech_tools-2.5.0-release.tar.gz | \
 tar xz --no-same-owner --no-same-permissions
-curl -L http://festvox.org/packed/festival/2.4/festival-2.4-release.tar.gz | \
+curl -L http://festvox.org/packed/festival/2.5/festival-2.5.0-release.tar.gz | \
+tar xz --no-same-owner --no-same-permissions
+curl -L http://festvox.org/flite/packed/flite-2.1/flite-2.1-release.tar.bz2 | \
 tar xz --no-same-owner --no-same-permissions
 
 echo "Building festival suit."
@@ -38,6 +40,10 @@ cd "${FESTIVAL_SUIT_PATH}/festival"
 
 # Build Festvox
 cd "${FESTIVAL_SUIT_PATH}/festvox"
+./configure && make
+
+# Build Flite
+cd "${FESTIVAL_SUIT_PATH}/flite"
 ./configure && make
 
 echo "Please add the following lines to your environment setting (.bashrc, .zshrc, etc.)"
